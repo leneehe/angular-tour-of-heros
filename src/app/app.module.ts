05338b95b3d9f1f333eb228d0b8e63838f5d33c3
Lena He <lhe@sensanetworks.com>
2018-05-16 16:25:46 -0400

    two-way binding with ngModule and import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
 
+import { FormsModule } from '@angular/forms'; // < -- NgModel lives here
+
 @NgModule({
   declarations: [
     AppComponent,
     HerosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
