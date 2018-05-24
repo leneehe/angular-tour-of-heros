6a7723c0b54f0b68ccc8373bb28ca05b6ea928a1
Lena He <lhe@sensanetworks.com>
2018-05-24 16:49:12 -0400

    Add deshboard and routings(default route)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HerosComponent } from './heros/heros.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
-  { path: 'heroes', component: HerosComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HerosComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ]
 
 @NgModule({
